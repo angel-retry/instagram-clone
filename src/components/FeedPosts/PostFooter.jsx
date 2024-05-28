b3a273490c1ecab9dc485f51582f8ffc74e5445a
unknown <a27657060@gmail.com>
2024-05-28 09:41:17 +0800

    feat: add like in PostFooter component

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -1,6 +1,29 @@
+import { Box, Flex } from '@chakra-ui/react'
+import { useState } from 'react'
+import { NotificationsLogo, UnlikeLogo } from '../../assets/constants'
+
 const PostFooter = () => {
+  const [liked, setLiked] = useState(false)
+  const [likes, setLikes] = useState(1000)
+
+  const handleLike = () => {
+    if (liked) {
+      setLiked(false)
+      setLikes(likes - 1)
+    } else {
+      setLiked(true)
+      setLikes(likes + 1)
+    }
+  }
+
   return (
-    <div>PostFooter</div>
+    <>
+      <Flex alignItems={'center'} gap={4} w={'full'} pt={0} mb={2} mt={'auto'}>
+        <Box onClick={handleLike}>
+          {!liked ? (<NotificationsLogo/>) : (<UnlikeLogo/>)}
+        </Box>
+      </Flex>
+    </>
   )
 }
 
