fe597f9c615efeb6fc2750c08ca6b154c4d1ff7f
unknown <a27657060@gmail.com>
2024-05-28 13:15:42 +0800

    feat: add avatar to ProfileHeader component

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -1,6 +1,12 @@
+import { Avatar, AvatarGroup, Flex } from '@chakra-ui/react'
+
 const ProfileHeader = () => {
   return (
-    <div>ProfileHeader</div>
+    <Flex gap={{ base: 4, sm: 10 }} py={10} direction={{ base: 'column', sm: 'row' }}>
+      <AvatarGroup size={{ base: 'xl', md: '2xl' }} justifySelf={'center'} alignSelf={'flex-start'} mx={'auto'}>
+        <Avatar name='As a Programmer' src='/profilepic.png' alt='As a Programmer' />
+      </AvatarGroup>
+    </Flex>
   )
 }
 
