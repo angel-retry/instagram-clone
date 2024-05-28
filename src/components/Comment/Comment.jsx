0908c9b12acbabd97e407e586b4fef33643e8185
unknown <a27657060@gmail.com>
2024-05-28 15:22:49 +0800

    feat: add content to Comment component to display user comment

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,6 +1,26 @@
-const Comment = () => {
+import { Avatar, Flex, Text } from '@chakra-ui/react'
+
+const Comment = ({ createdAt, username, profilePic, text }) => {
   return (
-    <div>Comment</div>
+    <Flex gap={4}>
+      <Avatar src={profilePic} name={username} size={'sm'} />
+
+      <Flex direction={'column'}>
+        <Flex gap={2} direction={{ base: 'column', lg: 'row' }}>
+          <Text fontWeight={'bold'} fontSize={12}>
+            {username}
+          </Text>
+
+          <Text fontSize={14}>
+            {text}
+          </Text>
+        </Flex>
+
+        <Text fontSize={12} color={'gray'}>
+          {createdAt}
+        </Text>
+      </Flex>
+    </Flex>
   )
 }
 
