ae04e57569726d8bf4f02beaab426b755ad6347a
unknown <a27657060@gmail.com>
2024-05-28 08:42:38 +0800

    feat: add content & styled in HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,17 @@
+import { Box, Container, Flex } from '@chakra-ui/react'
+
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <Container maxW={'container.lg'}>
+      <Flex gap={20}>
+        <Box flex={2} py={10} border={'1px solid blue'}>
+          FeedPosts
+        </Box>
+        <Box flex={3} display={{ base: 'none', lg: 'block' }} maxW={'300px'} border={'1px solid red'}>
+          Suggested Users
+        </Box>
+      </Flex>
+    </Container>
   )
 }
 
