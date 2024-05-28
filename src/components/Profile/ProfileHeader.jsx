e3d38390117a98cb0d144e80245a18a33de5da11
unknown <a27657060@gmail.com>
2024-05-28 13:29:49 +0800

    feat: add user name and edit profile button to ProfileHeader component

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -1,4 +1,4 @@
-import { Avatar, AvatarGroup, Flex } from '@chakra-ui/react'
+import { Avatar, AvatarGroup, Button, Flex, Text, VStack } from '@chakra-ui/react'
 
 const ProfileHeader = () => {
   return (
@@ -6,6 +6,25 @@ const ProfileHeader = () => {
       <AvatarGroup size={{ base: 'xl', md: '2xl' }} justifySelf={'center'} alignSelf={'flex-start'} mx={'auto'}>
         <Avatar name='As a Programmer' src='/profilepic.png' alt='As a Programmer' />
       </AvatarGroup>
+
+      <VStack alignItems={'start'} gap={2} mx={'auto'}
+       flex={1}>
+        <Flex
+          gap={4}
+          direction={{ base: 'column', sm: 'row' }}
+          justifyContent={{ base: 'center', sm: 'flex-start' }}
+          alignItems={'center'}
+          w={'full'}
+        >
+          <Text fontSize={{ base: 'sm', md: 'lg' }}>asaprogrammer_</Text>
+
+          <Flex gap={4} alignItems={'center'} justifyContent={'center'}>
+            <Button bg={'white'} color={'black'} _hover={{ bg: 'whiteAlpha.800' }} size={{ base: 'xs', md: 'sm' }}>
+              Edit Profile
+            </Button>
+          </Flex>
+        </Flex>
+      </VStack>
     </Flex>
   )
 }
