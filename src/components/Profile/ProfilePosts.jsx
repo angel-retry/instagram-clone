54cede217567f1821952ad4ba98a12193338b21d
unknown <a27657060@gmail.com>
2024-05-28 14:06:26 +0800

    feat: add skeleton, loading to ProfilePosts component

--- src/components/Profile/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts.jsx
@@ -1,6 +1,35 @@
+import { Box, Grid, Skeleton, VStack } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+
 const ProfilePosts = () => {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setInterval(() => setIsLoading(false), 2000)
+  }, [])
+
   return (
-    <div>ProfilePosts</div>
+    <Grid
+      templateColumns={{
+        sm: 'repeat(1, 1fr)',
+        md: 'repeat(3, 1fr)'
+      }}
+      gap={1}
+      columnGap={1}
+    >
+      {
+        isLoading && (
+          Array.from({ length: 5 }, (_, index) => (
+            <VStack key={index} alignItems={'flex-start'} gap={4}>
+              <Skeleton w={'full'}>
+                <Box h='300px'>content wrapped</Box>
+              </Skeleton>
+            </VStack>
+          ))
+        )
+      }
+
+    </Grid>
   )
 }
 
