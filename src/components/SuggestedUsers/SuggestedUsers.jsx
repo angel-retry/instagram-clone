40c308af97e3625a27a0753d9f8bf26aa647f421
unknown <a27657060@gmail.com>
2024-05-28 11:29:09 +0800

    feat: add content & styled in SuggestedUsers component

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,10 +1,32 @@
-import { VStack } from '@chakra-ui/react'
+import { Box, Flex, Text, VStack, Link } from '@chakra-ui/react'
 import SuggestedHeader from './SuggestedHeader'
+import SuggestedUser from './SuggestedUser'
 
 const SuggestedUsers = () => {
   return (
     <VStack py={8} px={6} gap={4}>
       <SuggestedHeader />
+
+      <Flex alignItems={'center'} justifyContent={'space-between'} w={'full'}>
+        <Text fontSize={12} fontWeight={'bold'} color={'gray.500'}>
+          Suggested for you
+        </Text>
+
+        <Text fontSize={12} fontWeight={'bold'} _hover={{ color: 'gray.400' }} cursor={'pointer'}>
+          See All
+        </Text>
+      </Flex>
+
+      <SuggestedUser />
+      <SuggestedUser />
+      <SuggestedUser />
+
+      <Box fontSize={12} color={'gray.500'} mt={5} alignSelf={'flex-start'}>
+        © 2023 Built By{' '}
+        <Link href='https://www.youtube.com/@asaprogrammer_' target='_blank' color={'blue.500'} fontSize={12}>
+          As a Programmer
+        </Link>
+      </Box>
     </VStack>
   )
 }
