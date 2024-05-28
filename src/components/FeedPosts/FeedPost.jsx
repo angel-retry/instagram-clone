7a28a3cc9db6d1bc63dbfa60c342762748d15bdf
unknown <a27657060@gmail.com>
2024-05-28 09:20:25 +0800

    feat: add FeedPosts, FeedPost, PostFooter, PostHeader components

---
+++ src/components/FeedPosts/FeedPost.jsx
@@ -0,0 +1,15 @@
+import { Box } from '@chakra-ui/react'
+import PostHeader from './PostHeader'
+import PostFooter from './PostFooter'
+
+const FeedPost = () => {
+  return (
+    <>
+      <PostHeader />
+      <Box></Box>
+      <PostFooter />
+    </>
+  )
+}
+
+export default FeedPost
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,12 @@
 import { Box, Container, Flex } from '@chakra-ui/react'
+import FeedPosts from '../../components/FeedPosts/FeedPosts'
 
 const HomePage = () => {
   return (
     <Container maxW={'container.lg'}>
       <Flex gap={20}>
         <Box flex={2} py={10} border={'1px solid blue'}>
-          FeedPosts
+          <FeedPosts />
         </Box>
         <Box flex={3} display={{ base: 'none', lg: 'block' }} maxW={'300px'} border={'1px solid red'}>
           Suggested Users
