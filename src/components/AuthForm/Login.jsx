f28affadb5251d88c4b1d54fb9b5dccde8489121
unknown <a27657060@gmail.com>
2024-05-29 09:40:23 +0800

    feat: add alert to Login component

--- src/components/AuthForm/Login.jsx
+++ src/components/AuthForm/Login.jsx
@@ -1,5 +1,6 @@
-import { Button, Input } from '@chakra-ui/react'
+import { Alert, AlertIcon, Button, Input } from '@chakra-ui/react'
 import { useState } from 'react'
+import useLogin from '../../hooks/useLogin'
 
 const Login = () => {
   const [inputs, setInputs] = useState({
@@ -7,6 +8,8 @@ const Login = () => {
     password: '',
     confirmPassword: ''
   })
+
+  const { login, loading, error } = useLogin()
 
   return (
     <>
@@ -27,7 +30,14 @@ const Login = () => {
         onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
       />
 
-      <Button w={'full'} colorScheme='blue' size={'sm'} fontSize={14} >
+      {error && (
+        <Alert status='error' fontSize={13} p={2} borderRadius={4}>
+          <AlertIcon fontSize={12} />
+          {error.message}
+        </Alert>
+      )}
+
+      <Button w={'full'} colorScheme='blue' size={'sm'} fontSize={14} onClick={() => login(inputs)} isLoading={loading}>
         Log in
       </Button>
     </>
