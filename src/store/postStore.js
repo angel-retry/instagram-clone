211956572d5429e0e35b84a76abf767cbf95d9a1
unknown <a27657060@gmail.com>
2024-05-30 19:23:05 +0800

    feat: create usePostComment hook

--- src/store/postStore.js
+++ src/store/postStore.js
@@ -4,7 +4,14 @@ const usePostStore = create(set => ({
   posts: [],
   createPost: post => set(state => ({ posts: [...state.posts, post] })),
   deletePost: id => set(state => ({ posts: state.posts.filter(post => post.id !== id) })),
-  setPosts: posts => set({ posts })
+  setPosts: posts => set({ posts }),
+  addComment: (postId, comment) => set(state => ({
+    posts: state.posts.map(post =>
+      post.id === postId
+        ? { ...post, comments: [...post.comments, comment] }
+        : post
+    )
+  }))
 }))
 
 export default usePostStore
