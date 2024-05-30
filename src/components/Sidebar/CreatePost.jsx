fd5744f24937b2e1b8582b83987224469f3164cb
unknown <a27657060@gmail.com>
2024-05-30 11:29:32 +0800

    feat: add Modal to CreatePost component

--- src/components/Sidebar/CreatePost.jsx
+++ src/components/Sidebar/CreatePost.jsx
@@ -1,31 +1,61 @@
-import { Box, Flex, Tooltip } from '@chakra-ui/react'
+import { Box, Button, Flex, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Textarea, Tooltip, useDisclosure } from '@chakra-ui/react'
 import { CreatePostLogo } from '../../assets/constants'
+import { BsFillImageFill } from 'react-icons/bs'
+import { useRef, useState } from 'react'
 
 const CreatePost = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const imageRef = useRef(null)
+
+  const [caption, setCaption] = useState(null)
+
   return (
-    <Tooltip
-      hasArrow
-      label={'Create'}
-      placement='right'
-      ml={1}
-      openDelay={500}
-      display={{ base: 'block', md: 'none' }}
-    >
-      <Flex
-        alignItems={'center'}
-        justifyContent={{ base: 'center', md: 'flex-start' }}
-        gap={4}
-        _hover={{ bg: 'whiteAlpha.400' }}
-        borderRadius={6}
-        p={2}
-        w={{ base: 10, md: 'full' }}
+    <>
+      <Tooltip
+        hasArrow
+        label={'Create'}
+        placement='right'
+        ml={1}
+        openDelay={500}
+        display={{ base: 'block', md: 'none' }}
       >
-        <CreatePostLogo size={25} />
-        <Box display={{ base: 'none', md: 'block' }}>
-          Create
-        </Box>
-      </Flex>
-    </Tooltip>
+        <Flex
+          alignItems={'center'}
+          justifyContent={{ base: 'center', md: 'flex-start' }}
+          gap={4}
+          _hover={{ bg: 'whiteAlpha.400' }}
+          borderRadius={6}
+          p={2}
+          w={{ base: 10, md: 'full' }}
+          onClick={onOpen}
+        >
+          <CreatePostLogo size={25} />
+          <Box display={{ base: 'none', md: 'block' }}>
+            Create
+          </Box>
+        </Flex>
+      </Tooltip>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent bg={'black'} solid={'1px solid gray'}>
+          <ModalHeader>Create Post</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <Textarea placeholder='Post caption' value={caption} onChange={e => setCaption(e.target.value)} />
+
+            <Input type='file' hidden ref={imageRef} />
+
+            <BsFillImageFill style={{ marginTop: '15px', marginLeft: '5px', cursor: 'pointer' }} size={16} onClick={() => imageRef.current.click()} />
+          </ModalBody>
+
+          <ModalFooter>
+            <Button mr={3}>Post</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+
   )
 }
 
