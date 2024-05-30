aed280994257305980bcad71f4ddef1ae4d3d722
unknown <a27657060@gmail.com>
2024-05-30 15:12:20 +0800

    feat: create useGetUserPosts hook

--- src/store/postStore.js
+++ src/store/postStore.js
@@ -2,7 +2,8 @@ import { create } from 'zustand'
 
 const usePostStore = create(set => ({
   posts: [],
-  createPost: post => set(state => ({ posts: [...state.posts, post] }))
+  createPost: post => set(state => ({ posts: [...state.posts, post] })),
+  setPosts: posts => set({ posts })
 }))
 
 export default usePostStore
