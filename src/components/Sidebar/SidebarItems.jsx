f73c8b1988564672241b26291c9aba0287c10fb7
unknown <a27657060@gmail.com>
2024-05-30 08:27:18 +0800

    feat: add Notifications to SidebarItems component

--- src/components/Sidebar/SidebarItems.jsx
+++ src/components/Sidebar/SidebarItems.jsx
@@ -1,9 +1,12 @@
 import Home from './Home'
+import Notifications from './Notifications'
 
 const SidebarItems = () => {
   return (
     <>
       <Home />
+      {/* Search */}
+      <Notifications />
     </>
   )
 }
