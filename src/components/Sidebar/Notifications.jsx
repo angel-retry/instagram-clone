c443465147566826e6c9c7038d60acae7d332a67
unknown <a27657060@gmail.com>
2024-05-30 08:25:44 +0800

    feat: create Notifications component

---
+++ src/components/Sidebar/Notifications.jsx
@@ -0,0 +1,32 @@
+import { Tooltip, Box, Flex } from '@chakra-ui/react'
+import { NotificationsLogo } from '../../assets/constants'
+
+const Notifications = () => {
+  return (
+    <Tooltip
+      hasArrow
+      label={'Notifications'}
+      placement='right'
+      ml={1}
+      openDelay={500}
+      display={{ base: 'block', md: 'none' }}
+    >
+      <Flex
+        alignItems={'center'}
+        justifyContent={{ base: 'center', md: 'flex-start' }}
+        gap={4}
+        _hover={{ bg: 'whiteAlpha.400' }}
+        borderRadius={6}
+        p={2}
+        w={{ base: 10, md: 'full' }}
+      >
+        <NotificationsLogo size={25} />
+        <Box display={{ base: 'none', md: 'block' }}>
+          Notifications
+        </Box>
+      </Flex>
+    </Tooltip>
+  )
+}
+
+export default Notifications
