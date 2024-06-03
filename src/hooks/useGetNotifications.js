5ed7f0d4adfbf75acb7461eecdb5cfaba4a2721e
unknown <a27657060@gmail.com>
2024-06-03 16:43:39 +0800

    feat: add refactor useGetNotifications with onSnapshot

--- src/hooks/useGetNotifications.js
+++ src/hooks/useGetNotifications.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import useAuthStore from '../store/authStore'
-import { collection, getDocs, query, where } from 'firebase/firestore'
+import { collection, onSnapshot, query, where } from 'firebase/firestore'
 import useShowToast from './useShowToast'
 import { firestore } from '../firebase/firebase'
 
@@ -11,30 +11,33 @@ const useGetNotifications = () => {
   const showToast = useShowToast()
 
   useEffect(() => {
-    const getNotifications = async () => {
-      setIsLoading(true)
-      const q = query(collection(firestore, 'notifications'), where('receiverId', '==', authUser.uid))
+    if (!authUser) return
 
-      try {
-        const querySnapShot = await getDocs(q)
-        const notifications = []
-        querySnapShot.forEach(doc => {
-          notifications.push({ id: doc.id, ...doc.data() })
-        })
+    setIsLoading(true)
+    const q = query(collection(firestore, 'notifications'), where('receiverId', '==', authUser.uid))
 
-        notifications.sort((a, b) => b.createdAt - a.createdAt)
+    // onSnapshot設置監聽器，當資料變更時會自動載入
+    // 將此函示給unsubscribe接收，當元件卸載時要觸發unsubscribe返回函示，結束監聽器
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const notifications = []
+      querySnapshot.forEach(doc => {
+        notifications.push({ id: doc.id, ...doc.data() })
+      })
+      notifications.sort((a, b) => b.createdAt - a.createdAt)
 
-        setNotifications(notifications)
-        console.log('notifications', notifications)
-      } catch (error) {
-        console.error(error)
-        showToast('Error', error.message, 'error')
-      } finally {
-        setIsLoading(false)
-      }
+      setNotifications(notifications)
+      setIsLoading(false)
+      console.log('notifications', notifications)
+    },
+    (error) => {
+      console.error(error)
+      showToast('Error', error.message, 'error')
+      setIsLoading(false)
     }
+    )
 
-    if (authUser) getNotifications()
+    // 元件卸载時執行 unsubscribe 函數，取消監聽器
+    return () => unsubscribe()
   }, [authUser, showToast, setNotifications])
 
   return { isLoading, notifications }
