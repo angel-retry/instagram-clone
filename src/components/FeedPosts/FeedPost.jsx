f60036dde38703fac6d1796f4c329f7ea6941406
unknown <a27657060@gmail.com>
2024-06-04 15:23:28 +0800

    feat: add Spinner to FeedPost

--- src/components/FeedPosts/FeedPost.jsx
+++ src/components/FeedPosts/FeedPost.jsx
@@ -1,18 +1,27 @@
-import { Box, Image } from '@chakra-ui/react'
+import { Box, Flex, Image, Spinner } from '@chakra-ui/react'
 import PostHeader from './PostHeader'
 import PostFooter from './PostFooter'
 import useGetUserProfileById from '../../hooks/useGetUserProfileById'
 
 const FeedPost = ({ post }) => {
-  const { userProfile } = useGetUserProfileById(post.createdBy)
-  console.log({ userProfile })
+  const { userProfile, isLoading } = useGetUserProfileById(post.createdBy)
   return (
     <>
-      <PostHeader post={post} creatorProfile={userProfile} />
-      <Box my={2} borderRadius={4} overflow={'hidden'}>
-        <Image src={post.imageURL} w={'full'} />
-      </Box>
-      <PostFooter post={post} creatorProfile={userProfile}/>
+    {isLoading
+      ? (
+        <Flex justifyContent={'center'} alignItems={'center'}>
+          <Spinner />
+        </Flex>
+        )
+      : (
+      <>
+        <PostHeader post={post} creatorProfile={userProfile} />
+        <Box my={2} borderRadius={4} overflow={'hidden'}>
+          <Image src={post.imageURL} w={'full'} />
+        </Box>
+        <PostFooter post={post} creatorProfile={userProfile}/>
+      </>
+        )}
     </>
   )
 }
