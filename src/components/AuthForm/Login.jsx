e2e139636a7539a6b51141ceeeff7189c3800f61
unknown <a27657060@gmail.com>
2024-06-04 16:41:09 +0800

    feat: add isInvalid to check inputs field is not empty to Login

--- src/components/AuthForm/Login.jsx
+++ src/components/AuthForm/Login.jsx
@@ -1,15 +1,33 @@
 import { Alert, AlertIcon, Button, Input } from '@chakra-ui/react'
 import { useState } from 'react'
 import useLogin from '../../hooks/useLogin'
+import useShowToast from '../../hooks/useShowToast'
 
 const Login = () => {
   const [inputs, setInputs] = useState({
     email: '',
-    password: '',
-    confirmPassword: ''
+    password: ''
   })
 
+  const [isInvalid, setIsInvalid] = useState(false)
+
   const { login, loading, error } = useLogin()
+  const showToast = useShowToast()
+
+  const handleLogin = async () => {
+    setIsInvalid(false)
+    if (!inputs.email || !inputs.password) {
+      setIsInvalid(true)
+      return showToast('Error', 'Please fill all th fields', 'error')
+    }
+    try {
+      await login(inputs)
+    } catch (error) {
+      return showToast('Error', error.message, 'error')
+    } finally {
+      setIsInvalid(false)
+    }
+  }
 
   return (
     <>
@@ -20,6 +38,7 @@ const Login = () => {
         val={inputs.email}
         size={'sm'}
         onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
+        isInvalid={isInvalid}
       />
       <Input
         placeholder='Password'
@@ -28,6 +47,7 @@ const Login = () => {
         val={inputs.password}
         size={'sm'}
         onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
+        isInvalid={isInvalid}
       />
 
       {error && (
@@ -37,7 +57,7 @@ const Login = () => {
         </Alert>
       )}
 
-      <Button w={'full'} colorScheme='blue' size={'sm'} fontSize={14} onClick={() => login(inputs)} isLoading={loading}>
+      <Button w={'full'} colorScheme='blue' size={'sm'} fontSize={14} onClick={handleLogin} isLoading={loading}>
         Log in
       </Button>
     </>
