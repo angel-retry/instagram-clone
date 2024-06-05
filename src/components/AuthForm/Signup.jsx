c46fd06c1a37b64390856cb5e04b8092a4a2618c
unknown <a27657060@gmail.com>
2024-06-05 10:57:43 +0800

    feat: add yup, useForm to implement checking user data to Signup

--- src/components/AuthForm/Signup.jsx
+++ src/components/AuthForm/Signup.jsx
@@ -1,58 +1,47 @@
+import * as yup from 'yup'
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
-import { Alert, AlertIcon, Button, Flex, FormControl, Input, InputGroup, InputRightElement } from '@chakra-ui/react'
+import { Alert, AlertIcon, Button, Flex, FormControl, FormErrorMessage, Input, InputGroup, InputRightElement } from '@chakra-ui/react'
 import { useState } from 'react'
 import useSignUpWithEmailAndPassword from '../../hooks/useSignUpWithEmailAndPassword'
 import useShowToast from '../../hooks/useShowToast'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+const schema = yup.object({
+  email: yup.string().email().required(),
+  username: yup.string().required(),
+  fullName: yup.string().required(),
+  password: yup.string()
+    .min(8, 'Password must be at least 8 characters long')
+    .max(16, 'Password must be not over 16 characters long')
+    .matches(/(?=.*[a-z])/, 'Password must contain at least one lowercase letter')
+    .matches(/(?=.*[A-Z])/, 'Password must contain at least one uppercase letter')
+    .matches(/(?=.*[0-9])/, 'Password must contain at least one number')
+    .required(),
+  confirmPassword: yup.string().oneOf([yup.ref('password')], 'Passwords must match').required()
+}).required()
 
 const Signup = () => {
   const [showPassword, setShowPassword] = useState(false)
-  const [inputs, setInputs] = useState({
-    username: '',
-    fullName: '',
-    email: '',
-    password: '',
-    confirmPassword: ''
-  })
 
-  const [isInvalids, setIsInvalids] = useState({
-    email: false,
-    username: false,
-    fullName: false,
-    password: false,
-    confirmPassword: false
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    mode: 'onTouched',
+    reValidateMode: 'onChange',
+    resolver: yupResolver(schema)
   })
 
   const { loading, error, signup } = useSignUpWithEmailAndPassword()
 
   const showToast = useShowToast()
 
-  const handleSignup = async () => {
-    setIsInvalids({
-      email: false,
-      username: false,
-      fullName: false,
-      password: false,
-      confirmPassword: false
-    })
+  const onSubmit = (data) => {
+    console.log({ data })
+  }
 
-    const fields = ['email', 'username', 'fullName', 'password', 'confirmPassword']
-
-    if (!inputs.email || !inputs.username || !inputs.fullName || !inputs.password || !inputs.confirmPassword) {
-      fields.forEach(field => {
-        if (!inputs[field]) {
-          setIsInvalids(prevState => ({
-            ...prevState,
-            [field]: true
-          }))
-        }
-      })
-      return showToast('Error', 'Please fill the field.', 'error')
-    }
-
-    if (inputs.password !== inputs.confirmPassword) {
-      setIsInvalids({ ...isInvalids, password: true, confirmPassword: true })
-      return showToast('Error', 'Please input the same password.', 'error')
-    }
+  const onError = (error) => {
+    if (!error) return
+    console.log(error)
+    showToast('Error', 'Please enter these fields', 'error')
   }
 
   return (
@@ -60,73 +49,61 @@ const Signup = () => {
       <Flex direction={'column'} gap={3}>
       <FormControl
         isRequired
-        isInvalid={ isInvalids.email }
+        isInvalid={errors.email}
       >
         <Input
           placeholder='Email'
           fontSize={14}
           type='email'
-          val={inputs.email}
+          name="email"
           size={'sm'}
-          onChange={(e) => {
-            setInputs({ ...inputs, email: e.target.value })
-            setIsInvalids({ ...isInvalids, email: false })
-          }}
+          {...register('email')}
         />
+        <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
       </FormControl>
 
       <FormControl
         isRequired
-        isInvalid={ isInvalids.email }
+        isInvalid={errors.username}
       >
         <Input
         placeholder='Username'
         fontSize={14}
         type='text'
-        val={inputs.username}
+        name='username'
+        {...register('username')}
         size={'sm'}
-        onChange={(e) => {
-          setInputs({ ...inputs, username: e.target.value })
-          setIsInvalids({ ...isInvalids, username: false })
-        }}
-        isInvalid={ isInvalids.username }
       />
+      <FormErrorMessage>{errors.username?.message}</FormErrorMessage>
       </FormControl>
 
       <FormControl
         isRequired
-        isInvalid={ isInvalids.email }
+        isInvalid={errors.fullName}
       >
         <Input
           placeholder='Full Name'
           fontSize={14}
           type='text'
-          val={inputs.fullName}
+          name='fullName'
+          {...register('fullName')}
           size={'sm'}
-          onChange={(e) => {
-            setInputs({ ...inputs, fullName: e.target.value })
-            setIsInvalids({ ...isInvalids, fullName: false })
-          }}
-          isInvalid={ isInvalids.fullName }
         />
+        <FormErrorMessage>{errors.fullName?.message}</FormErrorMessage>
       </FormControl>
 
       <FormControl
         isRequired
-        isInvalid={ isInvalids.email }
+        isInvalid={ errors.password }
       >
         <InputGroup>
           <Input
             placeholder='Password'
             fontSize={14}
             type={showPassword ? 'text' : 'password'}
-            val={inputs.password}
+            {...register('password')}
+            name='password'
             size={'sm'}
-            onChange={(e) => {
-              setInputs({ ...inputs, password: e.target.value })
-              setIsInvalids({ ...isInvalids, password: false })
-            }}
-            isInvalid={ isInvalids.password }
           />
 
           <InputRightElement h={'full'}>
@@ -135,24 +112,22 @@ const Signup = () => {
             </Button>
           </InputRightElement>
         </InputGroup>
+
+        <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
       </FormControl>
 
       <FormControl
         isRequired
-        isInvalid={ isInvalids.email }
+        isInvalid={ errors.confirmPassword }
       >
         <InputGroup>
           <Input
             placeholder='Confirm Password'
             fontSize={14}
             type={showPassword ? 'text' : 'password'}
-            val={inputs.confirmPassword}
+            {...register('confirmPassword')}
             size={'sm'}
-            onChange={(e) => {
-              setInputs({ ...inputs, confirmPassword: e.target.value })
-              setIsInvalids({ ...isInvalids, confirmPassword: false })
-            }}
-            isInvalid={ isInvalids.confirmPassword }
+            name='confirmPassword'
           />
 
           <InputRightElement h={'full'}>
@@ -161,6 +136,8 @@ const Signup = () => {
             </Button>
           </InputRightElement>
         </InputGroup>
+
+        <FormErrorMessage>{errors.confirmPassword?.message}</FormErrorMessage>
       </FormControl>
 
       {error && (
@@ -170,7 +147,7 @@ const Signup = () => {
         </Alert>
       )}
 
-      <Button w={'full'} colorScheme='blue' size={'sm'} fontSize={14} onClick={handleSignup} isLoading={loading}>
+      <Button w={'full'} colorScheme='blue' size={'sm'} fontSize={14} onClick={handleSubmit(onSubmit, onError)} isLoading={loading} isDisabled={errors.email || errors.username || errors.fullName || errors.password || errors.confirmPassword} >
         Sign Up
       </Button>
       </Flex>
