6857cdb4353d37166f6747b8d187627dd5431596
unknown <a27657060@gmail.com>
2024-06-05 11:45:04 +0800

    feat: refactor Login to check inputs

--- src/components/AuthForm/Login.jsx
+++ src/components/AuthForm/Login.jsx
@@ -1,66 +1,83 @@
-import { Alert, AlertIcon, Button, Input } from '@chakra-ui/react'
-import { useState } from 'react'
+import * as yup from 'yup'
+import { Alert, AlertIcon, Button, Flex, FormControl, FormErrorMessage, Input } from '@chakra-ui/react'
 import useLogin from '../../hooks/useLogin'
 import useShowToast from '../../hooks/useShowToast'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+const schema = yup.object({
+  email: yup.string().email().required(),
+  password: yup.string().required()
+}).required()
 
 const Login = () => {
-  const [inputs, setInputs] = useState({
-    email: '',
-    password: ''
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    mode: 'onTouched',
+    reValidateMode: 'onChange',
+    resolver: yupResolver(schema)
   })
-
-  const [isInvalid, setIsInvalid] = useState(false)
 
   const { login, loading, error } = useLogin()
   const showToast = useShowToast()
 
-  const handleLogin = async () => {
-    setIsInvalid(false)
-    if (!inputs.email || !inputs.password) {
-      setIsInvalid(true)
-      return showToast('Error', 'Please fill all th fields', 'error')
-    }
+  const onSumbit = async (data) => {
     try {
-      await login(inputs)
-    } catch (error) {
+      await login(data)
+    } catch (err) {
       return showToast('Error', error.message, 'error')
-    } finally {
-      setIsInvalid(false)
     }
   }
 
+  const onError = (error) => {
+    if (!error) return
+    showToast('Error', 'Please enter these fields', 'error')
+  }
+
   return (
-    <>
-      <Input
-        placeholder='Email'
-        fontSize={14}
-        type='email'
-        val={inputs.email}
-        size={'sm'}
-        onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
-        isInvalid={isInvalid}
-      />
-      <Input
-        placeholder='Password'
-        fontSize={14}
-        type='password'
-        val={inputs.password}
-        size={'sm'}
-        onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
-        isInvalid={isInvalid}
-      />
+    <form style={{ width: '100%' }}>
+      <Flex direction={'column'} gap={3}>
+        <FormControl
+          isInvalid={errors.email}
+        >
+          <Input
+            placeholder='Email'
+            fontSize={14}
+            type='email'
+            name='email'
+            size={'sm'}
+            {...register('email')}
+          />
 
-      {error && (
-        <Alert status='error' fontSize={13} p={2} borderRadius={4}>
-          <AlertIcon fontSize={12} />
-          {error.message}
-        </Alert>
-      )}
+          <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
+        </FormControl>
 
-      <Button w={'full'} colorScheme='blue' size={'sm'} fontSize={14} onClick={handleLogin} isLoading={loading}>
-        Log in
-      </Button>
-    </>
+        <FormControl
+           isInvalid={errors.password}
+        >
+          <Input
+            placeholder='Password'
+            fontSize={14}
+            type='password'
+            name='password'
+            size={'sm'}
+            {...register('password')}
+          />
+
+          <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
+        </FormControl>
+
+        {error && (
+          <Alert status='error' fontSize={13} p={2} borderRadius={4}>
+            <AlertIcon fontSize={12} />
+            {error.message}
+          </Alert>
+        )}
+
+        <Button w={'full'} colorScheme='blue' size={'sm'} fontSize={14} onClick={handleSubmit(onSumbit, onError)} isLoading={loading} isDisabled={errors?.email || errors?.password}>
+          Log in
+        </Button>
+      </Flex>
+    </form>
   )
 }
 
